c649e83d53d6f5c6b1c84877f1c45dfec7579883
berny1704 <bernardo.gzz95@gmail.com>
2025-04-07 19:32:19 -0600

    Descomentar codigo

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        Vehiculo carro = new Vehiculo("Mazda", 2025);
-//        carro.imprimirDatos();
+        Vehiculo carro = new Vehiculo("Mazda", 2025);
+        carro.imprimirDatos();
 
         Bicicleta bici = new Bicicleta("Apache", 2024);
         bici.imprimirDatos();
 
-//        Vehiculo carroUsado = new Vehiculo("Nissan", 2020, 50000);
-//        carroUsado.imprimirDatos();
+        Vehiculo carroUsado = new Vehiculo("Nissan", 2020, 50000);
+        carroUsado.imprimirDatos();
 
     }
 }
